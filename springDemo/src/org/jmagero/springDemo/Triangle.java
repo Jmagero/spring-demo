016662bb310ba6ebaa3af3ee5c950b6b0293bf0e
Jocyline Magero <jmagero54@gmail.com>
2022-02-25 10:52:37 -0600

    Injecting Objects

--- springDemo/src/org/jmagero/springDemo/Triangle.java
+++ springDemo/src/org/jmagero/springDemo/Triangle.java
@@ -1,34 +1,33 @@
 package org.jmagero.springDemo;
 
 public class Triangle {
-	private String type;
-	private int height;
-	
-	public int getHeight() {
-		return height;
-	}
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	public Triangle(String type) {
-		this.type = type;
+	public Point getPointA() {
+		return pointA;
 	}
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
-	public Triangle(int height) {
-		this.height = height;
+	public Point getPointB() {
+		return pointB;
 	}
-	
-	public String getType() {
-		return type;
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
 	}
-
-//	public void setType(String type) {
-//		this.type = type;
-//	}
+	public Point getPointC() {
+		return pointC;
+	}
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
 
 	public void draw() {
-		System.out.print(getType() + " has been drawn of height " + getHeight());
+		System.out.print("Point A ( " + getPointA().getX() + ", " + getPointA().getY() + " ) " +
+						"Point B ( " + getPointB().getX() + ", " + getPointB().getY() + " ) " +
+						"Point C ( " + getPointC().getX() + ", " + getPointC().getY() + " ) ");
 	}
 
 }
