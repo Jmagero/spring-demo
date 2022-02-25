7fa59452282de2ea1a8ba3f0a4ff8a46340dff37
Jocyline Magero <jmagero54@gmail.com>
2022-02-25 10:18:25 -0600

    Using constructor injection

--- springDemo/src/org/jmagero/springDemo/Triangle.java
+++ springDemo/src/org/jmagero/springDemo/Triangle.java
@@ -2,17 +2,33 @@ package org.jmagero.springDemo;
 
 public class Triangle {
 	private String type;
+	private int height;
+	
+	public int getHeight() {
+		return height;
+	}
+
+	public Triangle(String type) {
+		this.type = type;
+	}
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+	public Triangle(int height) {
+		this.height = height;
+	}
 	
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
+//	public void setType(String type) {
+//		this.type = type;
+//	}
 
 	public void draw() {
-		System.out.print(type + " has been drawn");
+		System.out.print(getType() + " has been drawn of height " + getHeight());
 	}
 
 }
