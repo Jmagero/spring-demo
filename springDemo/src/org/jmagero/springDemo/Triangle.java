d7fca707c8e5e9fd8955acb707b76fde620d967c
Jocyline Magero <jmagero54@gmail.com>
2022-02-25 10:05:07 -0600

    ApplicationContext and property initialization

--- springDemo/src/org/jmagero/springDemo/Triangle.java
+++ springDemo/src/org/jmagero/springDemo/Triangle.java
@@ -1,8 +1,18 @@
 package org.jmagero.springDemo;
 
 public class Triangle {
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public void draw() {
-		System.out.print("Triangle has been drawn");
+		System.out.print(type + " has been drawn");
 	}
 
 }
